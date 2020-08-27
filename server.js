ada8e9acf9489c1daaefdaa6f55d9716108bd795
Scott Davis <scott@thirstyhead.com>
2020-08-27 15:52:18 -0600

    POSTing hello world to zoom

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http = require('http');
+const https = require('https');
 const fs = require('fs');
 const { parse } = require('querystring');
+const url = require('url');
 
 const server = http.createServer( (request, response) => {
 
@@ -12,17 +14,12 @@ const server = http.createServer( (request, response) => {
     });
     
     request.on('end', () => {
-      console.dir(data);
       response.writeHead(200, {'Content-Type': 'text/html'});
-      // response.end('Closed captioning enabled.');
-      let output = '';
-      output += '<pre><code>Zoom meeting url:[';
-      requestBody = Buffer.concat(data).toString();
-      output += requestBody;
-      output += ']</code></pre>';
-      console.log(output);
-      response.end(output);
-      rr();
+      response.end('Closed captioning enabled.');
+      dataJson = parse( Buffer.concat(data).toString() );
+      zoomMeetingUrl = dataJson.meetingUrl;
+      post();
+      // rr(dataJson.meetingUrl);
     });
 
     request.on('error', (err) => {
@@ -41,21 +38,50 @@ const host = '127.0.0.1';
 server.listen(port, host);
 console.log(`Listening at http://${host}:${port}`);
 
-let lines = undefined;
-function rr(){
-  try{
-    const rawData = fs.readFileSync('./cc.txt');
-    const fileContents = rawData.toString();
-    lines = fileContents.split('\n');
-    setInterval(post, 1000);
-  } catch(err){
-    console.error(err);
-  }
-}
+// let lines = undefined;
+// function rr(incomingUrl){
+  // zoomMeetingUrl = incomingUrl;
 
+  // try{
+    // const rawData = fs.readFileSync('./cc.txt');
+    // const fileContents = rawData.toString();
+    // lines = fileContents.split('\n');
+    // setInterval(post, 1000);
+  // } catch(err){
+    // console.error(err);
+  // }
+// }
+
+let zoomMeetingUrl = undefined;
 function post(){
-  const line = lines.shift();
-  if(line){
-    console.log(line);
+  let urlParts = new URL(zoomMeetingUrl);
+  const data = 'Hello World';
+  const options = {
+    hostname: urlParts.hostname,
+    port: 443,
+    path: urlParts.pathname + urlParts.search + '&lang=en-US' + '&seq=1',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'plain/text',
+      'Content-Length': data.length
+    }
   }
+
+  const postToZoom = https.request(options, res => {
+    res.on('data', d => {
+      console.log(d.toString());
+    });
+  });
+
+  postToZoom.on('error', err => {
+    console.error(err);
+  });
+
+  postToZoom.write(data);
+  postToZoom.end();
+
+  // const line = lines.shift();
+  // if(line){
+    // console.log(line);
+  // }
 }
