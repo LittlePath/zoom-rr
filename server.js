83e2965e8ae9ac0e0bcd67a167764eafe29de4ce
Scott Davis <scott@thirstyhead.com>
2020-08-28 10:15:51 -0600

    invited Rick to the party

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const { parse } = require('querystring');
 const url = require('url');
 
+const port = 3000;
+const host = '127.0.0.1';
 const server = http.createServer( (request, response) => {
 
   if(request.method === 'POST') {
@@ -18,8 +20,7 @@ const server = http.createServer( (request, response) => {
       response.end('Closed captioning enabled.');
       dataJson = parse( Buffer.concat(data).toString() );
       zoomMeetingUrl = dataJson.meetingUrl;
-      post();
-      // rr(dataJson.meetingUrl);
+      setupCC();
     });
 
     request.on('error', (err) => {
@@ -33,55 +34,51 @@ const server = http.createServer( (request, response) => {
   }
 });
 
-const port = 3000;
-const host = '127.0.0.1';
 server.listen(port, host);
 console.log(`Listening at http://${host}:${port}`);
 
-// let lines = undefined;
-// function rr(incomingUrl){
-  // zoomMeetingUrl = incomingUrl;
-
-  // try{
-    // const rawData = fs.readFileSync('./cc.txt');
-    // const fileContents = rawData.toString();
-    // lines = fileContents.split('\n');
-    // setInterval(post, 1000);
-  // } catch(err){
-    // console.error(err);
-  // }
-// }
+//////////////////////////////////
 
 let zoomMeetingUrl = undefined;
-function post(){
-  let urlParts = new URL(zoomMeetingUrl);
-  const data = 'Hello World';
-  const options = {
-    hostname: urlParts.hostname,
-    port: 443,
-    path: urlParts.pathname + urlParts.search + '&lang=en-US' + '&seq=1',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'plain/text',
-      'Content-Length': data.length
-    }
-  }
-
-  const postToZoom = https.request(options, res => {
-    res.on('data', d => {
-      console.log(d.toString());
-    });
-  });
-
-  postToZoom.on('error', err => {
+let lines = undefined;
+let sequence = 1;
+function setupCC(){
+  try{
+    const rawData = fs.readFileSync('./cc.txt');
+    const fileContents = rawData.toString();
+    lines = fileContents.split('\n');
+    setInterval(post, 1000);
+  } catch(err){
     console.error(err);
-  });
+  }
+}
 
-  postToZoom.write(data);
-  postToZoom.end();
+function post(){
+  const data = lines.shift();
+  if(data){
+    const urlParts = new URL(zoomMeetingUrl);
+    const options = {
+      hostname: urlParts.hostname,
+      port: 443,
+      path: urlParts.pathname + urlParts.search + '&lang=en-US' + `&seq=${sequence++}`,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'plain/text',
+        'Content-Length': data.length
+      }
+    }
 
-  // const line = lines.shift();
-  // if(line){
-    // console.log(line);
-  // }
+    const postToZoom = https.request(options, res => {
+      res.on('data', d => {
+        console.log(d.toString());
+      });
+    });
+
+    postToZoom.on('error', err => {
+      console.error(err);
+    });
+
+    postToZoom.write(data);
+    postToZoom.end();
+  }
 }
